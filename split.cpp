ce1a200d32e1411c7efc0bad90884af6e6d7fd90
Cole Sevier <sevier@usc.edu>
2025-09-13 00:22:34 +0000

    finished with part 1

--- split.cpp
+++ split.cpp
@@ -14,10 +14,64 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
+// added prototype in split.h but here
+void append(Node*& in, Node* node);
+
+// helper function
+void append(Node*& in, Node* node){
+  // check to see if the list code is in is null
+  if (in == nullptr)
+  {
+    in = node;
+    // make the next pointer null
+    node->next = nullptr;
+  }
+  // recursive case
+  else
+  {
+    append(in->next, node);
+  }
+}
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+
+  // make a temp node to walk down and add to odds/evens
+  Node* temp = in;
+  // check to make sure that in isn't null
+  if (temp== nullptr)
+  {
+    return;
+  }
+  
+  // iterate in so that it doesn't change when we change temp
+  in = in->next;
+
+  // detach the node from the rest of in
+  temp->next = nullptr;
+  // start with the base cases
+  // Case 1: in is null
+
+  // Case 2: odd
+  if (temp->value%2 == 1)
+  {
+    // do a ptr switch with temp
+    // make sure it's in increasing order
+    append(odds, temp);
+    split(in, odds, evens);
+  }
+  // Case 3: even
+  else
+  {
+    // do a ptr switch with temp
+    append(evens, temp);
+    split(in, odds, evens);
+  }
+  // go to the next value of in
+  // go to recursive case
+
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -11,7 +11,43 @@ g++ split.cpp test_split.cpp -o test_split
 
 #include "split.h"
 
+// helper function for printing
+// void display(Node*& in)
+// {
+//   if (in->next == nullptr)
+//   {
+//     cout << 
+//   }
+// }
+
+// delete function
+void deletelist(Node*& node)
+{
+  if (node == nullptr)
+  {
+    return;
+  }
+  deletelist(node->next);
+  delete node;
+}
+
 int main(int argc, char* argv[])
 {
+  Node* in = nullptr;
+  Node* evens = nullptr;
+  Node* odds = nullptr;
+  // make nodes 1-3
+  Node* node1 = new Node(3, nullptr);
+  in = node1;
+  Node* node2 = new Node(24, nullptr);
+  node1->next = node2;
+  Node* node3 = new Node(107, nullptr);
+  node2->next = node3;
+  split(in, odds, evens);
+
+  deletelist(odds);
+  deletelist(evens);
+  deletelist(in);
+  // Go through odds and print all of the items (recursively)
 
 }
